d6eab4844b04a419f207d2acdc50045a81536c4b
haygcao <29725483+haygcao@users.noreply.github.com>
2025-02-17 22:21:34 +0800

    Update meiwakucheck.js

--- plugins/meiwakucheck.js
+++ plugins/meiwakucheck.js
@@ -62,7 +62,7 @@ function queryPhoneInfo(phoneNumber, requestId) {
     pluginId: pluginId,
     method: 'GET',
     requestId: requestId,
-    url: `https://www.meiwakucheck.com/search?tel=${phoneNumber}`,
+    url: `https://meiwakucheck.com/search?tel_no=${phoneNumber}`,
     headers: {
       "User-Agent": 'Mozilla/5.0 (Linux; arm_64; Android 14; SM-S711B) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.6723.199 YaBrowser/24.12.4.199.00 SA/3 Mobile Safari/537.36',
     },
