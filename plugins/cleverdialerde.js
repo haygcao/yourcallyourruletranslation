993611b5400a5af48d4431db49da7fc650fe6cd5
haygcao <29725483+haygcao@users.noreply.github.com>
2025-02-18 10:39:52 +0800

    Update cleverdialerde.js

--- plugins/cleverdialerde.js
+++ plugins/cleverdialerde.js
@@ -85,7 +85,7 @@ function queryPhoneInfo(phoneNumber, requestId) {
     pluginId: pluginId,
     method: 'GET',
     requestId: requestId,
-    url: `https://https://www.cleverdialer.de/telefonnummer/${phoneNumber}`,
+    url: `https://www.cleverdialer.de/telefonnummer/${phoneNumber}`,
     headers: {
       "User-Agent": 'Mozilla/5.0 (Linux; arm_64; Android 14; SM-S711B) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.6723.199 YaBrowser/24.12.4.199.00 SA/3 Mobile Safari/537.36',
     },
